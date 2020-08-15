b986867515a1c81ad01054be5e3b49d82776b411
David Ghazi <davidaghazi@gmail.com>
2020-08-14 21:34:03 -0400

    completed the checkbox for tasks

--- reacttodolist/src/App.js
+++ reacttodolist/src/App.js
@@ -13,21 +13,30 @@ class App extends Component {
       {
         id: 2,
         title: 'Have dinner',
-        completed: true
+        completed: false
       },
       {
-        id: 1,
+        id: 3,
         title: 'Meeting with boss',
         completed: false
       },
     ]
   }
 
+  markComplete = (id) => {
+    this.setState( {todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) } )
+  }
+
   render() {
     console.log(this.state.todos)
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
     </div>
     )
   }
--- reacttodolist/src/components/TodoItem.js
+++ reacttodolist/src/components/TodoItem.js
@@ -3,20 +3,23 @@ import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
     getStyle = () => {
-       if(this.props.todo.completed){
-           return {
-               textDecoration: 'line-through'
-           }
-       }else{
-           return{
-               textDecoration: 'none'
-           }
+       return {
+           backgroundColor: '#f4f4f4',
+           padding: '10px',
+           borderBottom: '1px #ccc dotted',
+           textDecoration: this.props.todo.completed ? 
+           'line-through' : 'none'
        }
     }
+
     render() {
+        const {id,title} = this.props.todo
         return (
             <div style={this.getStyle()}>
-                <p> { this.props.todo.title } </p>   
+                <p> 
+                    <input type="checkbox" onChange={this.props.markComplete.bind
+                    (this, id)} /> { ' ' }
+                    { title } </p>   
             </div>
         )
     }
@@ -24,11 +27,7 @@ export class TodoItem extends Component {
 
 //PropTypes
 TodoItem.propTypes = {
-    todos: PropTypes.array.isRequired
-}
-
-const itemStyle = {
-    backgroundColor: '#f4f4f4'
+    todo: PropTypes.object.isRequired
 }
 
 export default TodoItem
